852c448e93473a5f128c0493884295d9181d5734
Angelo Ricardo <angeloengcomp@gmail.com>
2022-11-23 18:20:54 -0300

    fix(home): remove link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Pegasus Mailer!</a>
+          Welcome to <a href="#">Pegasus Mailer!</a>
         </h1>
 
 
